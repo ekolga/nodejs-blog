68f26c8229f58bb81244d516012a447d89dffbab
kolga <kolga@kolesa.kz>
2021-10-04 21:41:41 +0600

    Finally the task is done

--- public/scripts/article-rate.js
+++ public/scripts/article-rate.js
@@ -1,18 +1,24 @@
-const likeButton         = document.querySelector('.post__full__like-button');
-const dislikeButton      = document.querySelector('.post__full__dislike-button');
-const likeButtonClass    = likeButton.className;
-const dislikeButtonClass = dislikeButton.className;
+const likeButtonClass    = '.post__full__like-button';
+const dislikeButtonClass = '.post__full__dislike-button';
+const likeButton         = document.querySelector(likeButtonClass);
+const dislikeButton      = document.querySelector(dislikeButtonClass);
+const emptyHeartIcon     = '/icons/heart.svg';
+const filledHeartIcon    = '/icons/heart-fill.svg';
+const emptySadIcon       = '/icons/emoji-sad.svg';
+const filledSadIcon      = '/icons/emoji-sad-fill.svg';
+const likeButtonImg      = document.querySelector(`${likeButtonClass} img`);
+const dislikeButtonImg   = document.querySelector(`${dislikeButtonClass} img`);
 const articleId          = window.location.pathname.split('/')[3];
-const user               = {
-    email: document.querySelector('input#email').value
-}
-let requestObj      = {
+const user               = {};
+user.email               = (document.querySelector('input#email')) ? document.querySelector('input#email').value : null
+let requestObj           = {
     method: 'POST',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: new URLSearchParams(user)
 }
+
 
 /**
  * Creates and updates the alert element
@@ -71,7 +77,7 @@ function updateClickEventListener(element, oldListenerMethod, newListenerMethod)
  * Disables button click events by adding them "pointer-events: none;" style
  */
 function disableButtonClickEvents() {
-    likeButton.className += ' disabled'
+    likeButton.className    += ' disabled'
     dislikeButton.className += ' disabled'
 }
 
@@ -101,6 +107,33 @@ async function parseResults(response) {
 }
 
 /**
+ * Changes like and dislike icons
+ * 
+ * @param {*} isLike 
+ * @param {*} toFill 
+ * @returns 
+ */
+function changeIcon(isLike, toFill) {
+    if (isLike) {
+        if (toFill) {
+            return likeButtonImg.src = filledHeartIcon;
+        }
+
+        return likeButtonImg.src = emptyHeartIcon;
+    } else {
+        if (toFill) {
+            return dislikeButtonImg.src = filledSadIcon;
+        }
+
+        return dislikeButtonImg.src = emptySadIcon;
+    }
+}
+
+function askForLogin() {
+    createAlertElement(false, 'Please, log in to rate this article')
+}
+
+/**
  * Sends the request for the like setting
  */
 async function sendSetLike() {
@@ -112,7 +145,8 @@ async function sendSetLike() {
 
     updateClickEventListener(likeButton, sendSetLike, sendUnsetLike);
     updateClickEventListener(dislikeButton, sendUnsetDislike, sendSetDislike);
-
+    changeIcon(true, true);
+    changeIcon(false, false);
     enableButtonEvents();
 }
 
@@ -128,7 +162,8 @@ async function sendSetDislike() {
 
     updateClickEventListener(dislikeButton, sendSetDislike, sendUnsetDislike);
     updateClickEventListener(likeButton, sendUnsetLike, sendSetLike);
-
+    changeIcon(false, true);
+    changeIcon(true, false);
     enableButtonEvents();
 }
 
@@ -143,7 +178,7 @@ async function sendUnsetLike() {
     await parseResults(response);
 
     updateClickEventListener(likeButton, sendUnsetLike, sendSetLike);
-
+    changeIcon(true, false);
     enableButtonEvents();
 }
 
@@ -158,22 +193,26 @@ async function sendUnsetDislike() {
     await parseResults(response);
 
     updateClickEventListener(dislikeButton, sendUnsetDislike, sendSetDislike);
-
+    changeIcon(false, false);
     enableButtonEvents();
 }
 
 /**
  * Initial event listeners
  */
-
-if (document.querySelector('.like-button__filled')) {
-    likeButton.addEventListener('click', sendUnsetLike)
+if (!user.email) {
+    likeButton.addEventListener('click', askForLogin)
+    dislikeButton.addEventListener('click', askForLogin)
 } else {
-    likeButton.addEventListener('click', sendSetLike)
-}
-
-if (document.querySelector('.dislike-button__filled')) {
-    dislikeButton.addEventListener('click', sendUnsetDislike)
-} else {
-    dislikeButton.addEventListener('click', sendSetDislike)
+    if (document.querySelector('.like-button__filled')) {
+        likeButton.addEventListener('click', sendUnsetLike)
+    } else {
+        likeButton.addEventListener('click', sendSetLike)
+    }
+    
+    if (document.querySelector('.dislike-button__filled')) {
+        dislikeButton.addEventListener('click', sendUnsetDislike)
+    } else {
+        dislikeButton.addEventListener('click', sendSetDislike)
+    }
 }
